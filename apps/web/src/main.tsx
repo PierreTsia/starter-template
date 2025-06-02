f216c1857a8cdf62b8405ae5e47a9b23991b7c8d
Pierre_T <pierre.tsiakkaros@gmail.com>
2025-06-03 00:08:48 +0200

    feat(frontend): add minimal React Router setup

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import App from './App';
-
 import './index.css';
+
+function About() {
+  return <h2>About Page</h2>;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <nav style={{ marginBottom: 16 }}>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
