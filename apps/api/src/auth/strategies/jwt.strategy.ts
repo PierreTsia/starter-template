a7fe344bb7635465e12f7c9ff4029b240448804b
Pierre_T <pierre.tsiakkaros@gmail.com>
2025-06-05 22:17:49 +0200

    fall back to Bearer token as chrome desktop is breaking our balls

--- apps/api/src/auth/strategies/jwt.strategy.ts
+++ apps/api/src/auth/strategies/jwt.strategy.ts
@@ -1,25 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 import { UsersService } from '../../users/users.service';
-
-interface RequestWithCookies extends Request {
-  cookies: {
-    token?: string;
-  };
-}
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private usersService: UsersService) {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: RequestWithCookies) => {
-          return request?.cookies?.token || null;
-        },
-      ]),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET || 'super-secret',
     });
