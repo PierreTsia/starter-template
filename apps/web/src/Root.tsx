b59f02c72363e6fa2d2e9cd603e414340918871d
Pierre_T <pierre.tsiakkaros@gmail.com>
2025-06-11 10:15:21 +0200

    clean unused components

--- apps/web/src/Root.tsx
+++ apps/web/src/Root.tsx
@@ -1,13 +1,9 @@
-import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import App from './App';
 import { AboutPage } from './components/AboutPage';
-import { BuggyCounter } from './components/BuggyCounter';
-import { LoadingSpinner } from './components/LoadingSpinner';
 import { NotFoundPage } from './components/NotFoundPage';
 import { SettingsPage } from './components/SettingsPage';
-import { TanstackDemo } from './components/TanstackDemo';
 import { AuthPage } from './components/auth/AuthPage';
 import { ConfirmEmailPage } from './components/auth/ConfirmEmailPage';
 import { EmailConfirmationPage } from './components/auth/EmailConfirmationPage';
@@ -65,24 +61,7 @@ export const Root = () => {
               </ProtectedRoute>
             }
           />
-          <Route
-            path="/buggy"
-            element={
-              <ProtectedRoute>
-                <BuggyCounter />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/tanstack-demo"
-            element={
-              <ProtectedRoute>
-                <Suspense fallback={<LoadingSpinner />}>
-                  <TanstackDemo />
-                </Suspense>
-              </ProtectedRoute>
-            }
-          />
+
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
